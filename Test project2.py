7ba5a2a0567b97358909b2505e9faf4cf8359e21
PigJerry <04258wangzhao@gmail.com>
2026-08-20 22:13:10 +0800

    8.20新增文件1

---
+++ Test project2.py
@@ -0,0 +1,14 @@
+list_test = [1, 2, 3, 4, 5]
+
+for out in list_test:
+    print(out)
+
+list_test.append(6)
+
+for out in list_test:
+    print(out)
+
+list_test.insert(2, 2.5)
+
+for out1 in list_test:
+    print(out1)
